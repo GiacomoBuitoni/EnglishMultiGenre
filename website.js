ebdfcad971f3201469cdfe8a541bb756538fe988
GiacomoBuitoni <79340880+GiacomoBuitoni@users.noreply.github.com>
2023-06-02 12:04:59 -0400

    Update website.js

--- website.js
+++ website.js
@@ -5,15 +5,16 @@ document.getElementById("accessButton").onclick = function(){
     if(document.getElementById("userInput").value === accessKey){
         evalResult.innerHTML = "GRANTED"
         evalResult.style.fontSize = '90px';
+        evalResult.style.color = 'rgb(24, 233, 24)';
         document.getElementById("startInfo").style.display = "none";
         setTimeout(() => {progressBar.style.setProperty("visibility", "visible");}, 1000)
         
         var width;
-        setTimeout(() => {var loading = setInterval(() => {
+        var loading = setInterval(() => {
             const computedStyle = getComputedStyle(progressBar);
             width = parseFloat(computedStyle.getPropertyValue ('--width')) || 0
             progressBar.style.setProperty("--width", width + .1)
-            if(width == 100){
+            if(width >= 100){
                 clearInterval(loading);
                 console.log("DONE")
                 progressBar.style.setProperty("visibility", "hidden"); 
@@ -22,18 +23,21 @@ document.getElementById("accessButton").onclick = function(){
             }
             console.log(width)
         
-        }, 5)}, 1000)
+        }, 5)
         
         
 
     }
-
-
-    // if(parseInt(progressBarDat.getPropertyValue ('--width')) == 100){
-    //     console.log("DONE")
-    //     progressBar.style.setProperty("visibility", "hidden"); 
-    //     evalResult.style.display = "none"
-    // }
-}
-
-//left: 35%; top:300px;
+    else{
+        var denier = false;
+        evalResult.innerHTML = "DENIED"
+        evalResult.style.fontSize = '90px';
+        evalResult.style.color = "red";
+        document.getElementById("startInfo").style.display = "none";
+        setTimeout(() => {
+            document.getElementById("startInfo").style.display = "block";
+            evalResult.innerHTML = ''
+            document.getElementById("userInput").innerText = '';
+        }, 5000)
+        
+    }
