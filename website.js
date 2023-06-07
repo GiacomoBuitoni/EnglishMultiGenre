173a4de7ddfc247755806b28566f8a73d83e9e2f
GiacomoBuitoni <79340880+GiacomoBuitoni@users.noreply.github.com>
2023-06-07 19:12:08 -0400

    Update website.js

--- website.js
+++ website.js
@@ -1,4 +1,4 @@
-var accessKey = "A745EER"
+var accessKey = "BfBOYS52242"
 var evalResult = document.getElementById("evaluation")
 var progressBar = document.getElementsByClassName("progress-bar")[0]
 document.getElementById("accessButton").onclick = function(){
